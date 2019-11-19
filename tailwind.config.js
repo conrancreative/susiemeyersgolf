9ba4848ac37565bb2d4d55b5a309ab16764f5915
tconran <conrancreative@gmail.com>
2019-11-18 21:53:22 -0700

    homepage work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,78 @@
 module.exports = {
   theme: {
+    transform: {
+      // defaults to this value
+      none: "none"
+    },
+    transformOrigin: {
+      // defaults to these values
+      t: "top",
+      tr: "top right",
+      r: "right",
+      br: "bottom right",
+      b: "bottom",
+      bl: "bottom left",
+      l: "left",
+      tl: "top left"
+    },
+    translate: {
+      // defaults to {}
+      "1/2": "50%",
+      full: "100%",
+      "right-up": ["100%", "-100%"],
+      "3d": ["40px", "-60px", "-130px"]
+    },
+    scale: {
+      // defaults to {}
+      "90": "0.9",
+      "100": "1",
+      "110": "1.1",
+      "-100": "-1",
+      "stretched-x": ["2", "0.5"],
+      "stretched-y": ["0.5", "2"],
+      "3d": ["0.5", "1", "2"]
+    },
+    rotate: {
+      // defaults to {}
+      "90": "90deg",
+      "180": "180deg",
+      "270": "270deg",
+      "3d": ["0", "1", "0.5", "45deg"],
+      "5": "5deg",
+      "-5": "-5deg"
+    },
+    skew: {
+      // defaults to {}
+      "-5": "-5deg",
+      "5": "5deg"
+    },
+    perspective: {
+      // defaults to {}
+      none: "none",
+      "250": "250px",
+      "500": "500px",
+      "750": "750px",
+      "1000": "1000px"
+    },
+    perspectiveOrigin: {
+      // defaults to these values
+      t: "top",
+      tr: "top right",
+      r: "right",
+      br: "bottom right",
+      b: "bottom",
+      bl: "bottom left",
+      l: "left",
+      tl: "top left"
+    },
+
+    borderRadius: {
+      sm: "0.25rem",
+      md: "0.5rem",
+      lg: "0.75rem",
+      xl: "2rem",
+      full: "999px"
+    },
     extend: {
       colors: {
         "primary-red": {
@@ -84,9 +157,43 @@ module.exports = {
   },
   variants: {
     opacity: ["hover", "responsive"],
-    textColor: ["active", "focus", "group-hover", "hover", "responsive"],
+    textColor: [
+      "active",
+      "focus",
+      "group-hover",
+      "hover",
+      "responsive",
+      "focus"
+    ],
     margin: ["last", "first", "odd", "even", "responsive"],
-    backgroundColor: ["last", "first", "odd", "even", "responsive"]
+    backgroundColor: [
+      "last",
+      "first",
+      "odd",
+      "even",
+      "responsive",
+      "hover",
+      "active",
+      "focus"
+    ],
+    transform: ["responsive"],
+    transformOrigin: ["responsive"],
+    translate: ["responsive"],
+    scale: ["responsive"],
+    rotate: ["responsive"],
+    skew: ["responsive"],
+    perspective: ["responsive"],
+    perspectiveOrigin: ["responsive"],
+    transformStyle: ["responsive"],
+    backfaceVisibility: ["responsive"],
+    transformBox: ["responsive"],
+    borderStyle: ["responsive", "hover", "active", "focus"],
+    padding: ["first", "last", "odd", "even", "responsive"],
+    group: ["hover"]
   },
-  plugins: []
+  plugins: [
+    require("tailwindcss-transforms")({
+      "3d": false // defaults to false
+    })
+  ]
 };
